481276949d697a92a0a43cea54c8503baddc59a8
Victor <victor.nas2006@gmail.com>
2025-07-15 18:17:00 -0700

    OAuth working in sandbox environemnt

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ async function Auth() {
     const code_verifier = sessionStorage.getItem("code_verifier");
 
     //Sandbox Access Token
-    const myUrl = new URL(window.location.href.replace(/#/g, "?"));
-    const accessToken = myUrl.searchParams.get("access_token");
+    accessToken = window.location.hash.substring(14);
+    console.log("Access Token: " + accessToken);
 
     if (code && code_verifier) {
         fetch(cloverOAuthURL + "/oauth/token", {
@@ -30,13 +30,13 @@ async function Auth() {
                 grant_type: "authorization_code",
                 code: code,
                 redirect_uri: 'https://catmangamer.github.io/Order-to-PDF/',
-                client_id: "J1G5HH7DNR95M",
+                client_id: clientID,
                 code_verifier: code_verifier
             })
         })
             .then(res => res.json())
             .then(data => {
-                const { access_token, merchant_id, refresh_token } = data;
+                var { access_token, merchant_id, refresh_token } = data;
                 accessToken = access_token;
                 merchantId = merchant_id;
                 refreshToken = refresh_token;
@@ -46,6 +46,16 @@ async function Auth() {
                 sessionStorage.setItem("refresh_token", refreshToken);
             })
             .catch(err => console.error("Token exchange failed", err));
+    }
+    else if (code) {
+        fetch(cloverOAuthURL + "/oauth/token", {
+            method: "POST",
+            body: {
+                client_id: clientID,
+                client_secret: '720c8ac5- 22be- 02f9 - fb42 - 3a4a0a621fab',
+                code: code
+            }
+        })
     }
     else if (!accessToken){
         document.getElementById("status").textContent = "Redirecting to Clover for OAuth...";
@@ -74,31 +84,57 @@ async function generatePKCECodes() {
 
 
 async function getOrders() {
+    console.log("Access Token: "+accessToken)
     const orderDat = {
         method: 'GET',
         headers: { accept: 'application/json', authorization: 'Bearer ' + accessToken }
     };
     fetch(cloverAPIURL+'/v3/merchants/' + merchantId + '/orders', orderDat)
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => document.getElementById("orders").innerHTML = res)
+        .then(res => console.log("Order Data: " + JSON.stringify(res)))
         .catch(err => console.error(err));
-    document.getElementById("orders").innerHTML = orderDat;
-    console.log(orderDat);
-    const data = await orderDat.json();
     try {
         const res = await fetch(cloverAPIURL+'/v3/merchants/'+merchantId+'/orders', orderDat);
         const json = await res.json();
         console.log(json);
-        document.getElementById("orderList").textContent = JSON.stringify(json, null, 2);
+        document.getElementById("orderList").textContent = JSON.stringify(json);
     } catch (err) {
         console.error("Failed to fetch orders:", err);
     }
 }
 
+async function makeOrder(typeOrder) {
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            authorization: 'Bearer c1071db9-1643-8c9b-73f6-8ee20c733c18'
+        },
+        body: JSON.stringify({
+            orderCart: {
+                orderType: {
+                    taxable: 'false',
+                    isDefault: 'false',
+                    filterCategories: 'false',
+                    isHidden: 'false',
+                    isDeleted: 'false'
+                },
+                groupLineItems: 'false'
+            }
+        })
+    };
 
+    fetch('https://sandbox.dev.clover.com/v3/merchants/B3MRAVHXRY7C1/atomic_order/orders', options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
 
 async function generatePDF() {
     const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
+    const doc = new jsPDF({});
+    doc.output('dataurlnewwindow');
     doc.save("order.pdf");
 }
